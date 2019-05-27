f7ccd0d30e7c6a590ee181fa4ccfb34f236ac335
zhangxi <zhangxi@youzan.com>
2019-05-27 10:27:01 +0800

    微信百度解构语法转换

--- src/bd/loader.js
+++ src/bd/loader.js
@@ -12,6 +12,9 @@ module.exports = class BdLoaderHelper {
     // 更换指令前缀
     content = content.replace(/wx:/g, 's-')
 
+    // 更换解构语法
+    content = content.replace(/(?<=(?:<template[^>]*))\bdata\b\s*=\s*[\'\"]?{{\s*([^\'\"]*)\s*}}[\'\"]?/gi, `data="{{{$1}}}"`)
+
     // 替换模板函数
     content = content.replace(/<wxs/g, '<filter')
     content = content.replace(/wxs>/g, 'filter>')
