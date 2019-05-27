6dc6d038340f8a2069a71ff29b930023804732fb
zhangxi <zhangxi@youzan.com>
2019-05-27 16:02:59 +0800

    fix: wxs模块格式转换

--- src/bd/loader.js
+++ src/bd/loader.js
@@ -18,66 +18,8 @@ module.exports = class BdLoaderHelper {
     // 替换模板函数
     content = content.replace(/<wxs/g, '<filter')
     content = content.replace(/wxs>/g, 'filter>')
-
-    // 百度 filter 使用的是 es6 模块规范
-    content = this.transformWxs(content);
+    content = content.replace(/module\.exports\s{0,}=/, 'export default')
 
     return content
-  }
-
-  transformWxs(str) {
-    let newStr = '';
-    let filterOpenName = '';
-    let filterCloseName = '';
-    let codeBody = '';
-    let filterOpenStart = -1;
-    let filterOpenEnd = -1;
-    let filterCloseStart = -1;
-    let filterCloseEnd = -1;
-    let code = str;
-    let seg = [];
-    const wxs = [];
-
-    while(code) {
-      seg = [];
-      filterOpenStart = code.indexOf('<filter');
-
-      if (filterOpenStart > -1) {
-        seg.push(code.slice(0, filterOpenStart));
-
-        filterOpenEnd = code.indexOf('>', filterOpenStart) + 1;
-        filterOpenName = code.slice(filterOpenStart, filterOpenEnd);
-
-        seg.push(filterOpenName);
-        seg.push('\nvar exportFunc = {};\n');
-        code = code.slice(filterOpenEnd);
-
-        filterCloseStart = code.indexOf('</filter>');
-
-        if (filterCloseStart > -1) {
-          codeBody = code.slice(0, filterCloseStart);
-          codeBody = codeBody.replace(/module\.exports/g, 'exportFunc');
-          seg.push(codeBody);
-          seg.push('\nexport default = exportFunc;\n');
-
-          filterCloseEnd = filterCloseStart + 9;
-          filterCloseName = code.slice(filterCloseStart, filterCloseEnd);
-          seg.push(filterCloseName);
-
-          code = code.slice(filterCloseEnd);
-        }
-
-        wxs.push(seg);
-      } else {
-        wxs.push([code]);
-        break;
-      }
-    }
-
-    wxs.forEach(seg => {
-      newStr += `${seg.join('')}\n`;
-    });
-
-    return newStr;
   }
 }
