1419b6ff6e5e9ff07ca0191942ab650d2cc4f07f
zhangxi <zhangxi@youzan.com>
2019-05-30 21:08:06 +0800

    修改替换解构正则

--- src/bd/loader.js
+++ src/bd/loader.js
@@ -13,7 +13,7 @@ module.exports = class BdLoaderHelper {
     content = content.replace(/wx:/g, 's-')
 
     // 更换解构语法
-    content = content.replace(/(?<=(?:<template[^>]*))\bdata\b\s*=\s*[\'\"]?{{\s*([^\'\"]*)\s*}}[\'\"]?/gi, `data="{{{$1}}}"`)
+    content = content.replace(/(?<=(?:<template[^>]*))\bdata\b\s*=\s*[\"]?{{\s*([^\"]*)\s*}}[\'\"]?/gi, 'data="{{{$1}}}"')
 
     // 替换模板函数
     content = content.replace(/<wxs/g, '<filter')
